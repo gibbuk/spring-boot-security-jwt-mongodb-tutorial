a2492fb586b17dd5ab619167405f8966b3e5ea4f
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-04 16:23:47 +0000

    Add security configuration code

---
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.bezkoder.spring.jwt.mongodb.security.services;
+
+import com.bezkoder.spring.jwt.mongodb.models.User;
+import com.bezkoder.spring.jwt.mongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
