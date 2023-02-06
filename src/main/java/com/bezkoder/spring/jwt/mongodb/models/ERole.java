c76467e31d9daba6be2238a45bd05d9b819d67e1
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-06 09:36:51 +0000

    Add server routes - user authentication and user role checking for content access

--- src/main/java/com/bezkoder/spring/jwt/mongodb/models/ERole.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/models/ERole.java
@@ -2,6 +2,6 @@ package com.bezkoder.spring.jwt.mongodb.models;
 
 public enum ERole {
     ROLE_USER,
-    Role_MODERATOR,
+    ROLE_MODERATOR,
     ROLE_ADMIN
 }
--- src/main/java/com/bezkoder/spring/jwt/mongodb/payload/request/SignupRequest.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/payload/request/SignupRequest.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 import javax.validation.constraints.*;
 
-public class SignUpRequest {
+public class SignupRequest {
     @NotBlank
     @Size(min = 3, max = 20)
     private String username;
