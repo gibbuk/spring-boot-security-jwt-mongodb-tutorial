2d6e52db732169bab7d620e249a36dc2545a28e6
Alistair Gibb <aligibb_uk@hotmail.com>
2023-02-06 10:27:59 +0000

    Update UserDetailsServiceImpl.java - add @Service annotation

--- src/main/java/com/bezkoder/spring/jwt/mongodb/security/services/UserDetailsServiceImpl.java
+++ src/main/java/com/bezkoder/spring/jwt/mongodb/security/services/UserDetailsServiceImpl.java
@@ -1,13 +1,16 @@
 package com.bezkoder.spring.jwt.mongodb.security.services;
 
-import com.bezkoder.spring.jwt.mongodb.models.User;
-import com.bezkoder.spring.jwt.mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bezkoder.spring.jwt.mongodb.models.User;
+import com.bezkoder.spring.jwt.mongodb.repository.UserRepository;
+
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
